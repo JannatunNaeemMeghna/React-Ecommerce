8f40a041afbbf84d775b21d6a06d0b1427fde8f7
JannatunNaeemMeghna <17103324@iubat.edu>
2020-11-18 23:31:01 +0600

    navbar added using react-bootstrap and template from bootswatch, also used cdnjs font awesome library

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
-import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Welcome to Our Shop</h1>
-      <h2>Find all authentic product in the most reasonable price</h2>
+    <>
+      <Header />
+        <main className='py-3'>
+        <Container>
+        <h1>Welcome to Hardware Next Door</h1>
+        <h2>Find all authentic product in the most reasonable price</h2>
+        </Container>
+        </main>
+      <Footer />
 
-    </div>
+    </>
   );
 }
 
