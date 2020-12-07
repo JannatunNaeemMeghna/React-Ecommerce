516ab0f69b4f7e2c005388c88069d28f7d5d2e7f
JannatunNaeemMeghna <17103324@iubat.edu>
2020-12-07 20:59:39 +0600

    front-end with product images

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Homescreen from './screens/Homescreen'
 
 
 const App = () => {
+
   return (
     <>
       <Header />
         <main className='py-3'>
         <Container>
-        <h1>Welcome to Hardware Next Door</h1>
-        <h2>Find all authentic product in the most reasonable price</h2>
+          <Homescreen />
         </Container>
         </main>
       <Footer />
