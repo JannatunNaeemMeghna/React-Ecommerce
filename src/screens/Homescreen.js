807bdb9b4939024a9efd28b290ed577c9609f57c
JannatunNaeemMeghna <17103324@iubat.edu>
2020-12-19 01:22:50 +0600

    with models and routes

--- src/screens/Homescreen.js
+++ src/screens/Homescreen.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import Axios from 'axios'
+import React, { useState, useEffect} from 'react'
 import { Row, Col } from 'react-bootstrap'
-import products from '../products'
 import Product from '../components/Product'
 
 const Homescreen = () => {
+
+    const [products, setProducts] = useState([]) 
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+           const{ data } = await Axios.get('/api/products')
+
+           setProducts(data)
+        }
+
+        fetchProducts()
+    }, [])
+
     return (
         <>
             <h1>Latest Products</h1>
             <Row>
                 {products.map(product => (
-                    <Col sm={12} md={6} lg={4} xl={3}>
+                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                        <Product product={product} />
                     </Col>
                 ))}
